3ad9e4ff57e44a668897980c16c14f7c629f0662
PhilBligh <philbligh@hotmail.com>
2022-02-16 13:39:19 +0000

    finished exercises

--- ruby-fundamentals/class-syntax/exercise1/lib/car.rb
+++ ruby-fundamentals/class-syntax/exercise1/lib/car.rb
@@ -1,2 +1,18 @@
 class Car
+  def initialize
+    @speed = 0
+  end
+
+  def speed
+    return @speed
+  end
+
+  def accelerate(n)
+    @speed += n
+  end
+
+  def brake(n)
+    @speed -= n
+  end
+
 end
