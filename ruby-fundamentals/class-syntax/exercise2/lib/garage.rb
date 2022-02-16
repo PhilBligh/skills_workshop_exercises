5c824bc98bd2d9f8a5f40e09aedeac6c6f269237
PhilBligh <philbligh@hotmail.com>
2022-02-16 13:32:12 +0000

    Class exercises finished

--- ruby-fundamentals/class-syntax/exercise2/lib/garage.rb
+++ ruby-fundamentals/class-syntax/exercise2/lib/garage.rb
@@ -6,4 +6,41 @@ class Garage
       {registration_plate: 'SE60 TSW', make: 'honda'}
     ]
   end
+
+  def cars
+    @cars
+  end
+
+  def add(car)
+    @cars.push(car)
+  end
+
+  def find(reg)
+    @cars.each {|car|
+      if car[:registration_plate] == reg
+        return car
+      end
+    }
+  end
+
+  def remove(reg)
+    @cars.each {|car|
+      if car[:registration_plate] == reg
+        @cars.delete(car)
+      end
+    }
+  end
+
+  def all_of_make(make)
+    make_arr = []
+    @cars.each {|car|
+      if car[:make] == make
+        make_arr << car
+      end
+    }
+    make_arr
+  end
+
 end
+garage = Garage.new
+garage.cars
